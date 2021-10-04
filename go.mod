48545beb406a5b32bad5a68a513247a05611b94b
frev <frev@itu.dk>
2021-10-04 12:00:32 +0200

    Back to square one

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/JeppeKorgaard98/mAssignment1 //maybe it shouldb e called: mAssignment1 
+
+go 1.17
---
+++ server/main.go
@@ -0,0 +1,43 @@
+package server
+
+import (
+	"context"
+	"log"
+	"net"
+
+	course "mAssignment1/course" ///Users/Freja/Documents/Programmering/3.semester/disys/Mandatory1/mAssignment1
+
+	"google.golang.org/grpc"
+)
+
+type server struct {
+	course.UnimplementedCoursesServer
+}
+
+//the type of data we can handle
+type Course struct {
+	Id                        int32  `json:"id"`
+	Name                      string `json:"name"`
+	Teacher                   int32  `json:"teacher"`
+	StudentSatisfactionRating int32  `json:"studentSatisfactionRating,omitempty"`
+}
+
+func (s *server) GetCourseList(ctx context.Context, in *course.GetCourseListRequest) (*course.GetCourseListReply, error) {
+	return &course.GetCourseListReply{Message: "Here goes the list of courses ;)"}, nil //.HelloReply{Message: fmt.Sprint(courses[num].Name) + ";" + fmt.Sprint(courses[num].StudentSatisfactionRating)}, nil
+}
+
+func main() {
+	//port :8080
+	lis, err := net.Listen("tcp", ":8080")
+
+	if err != nil { //error before listening
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer() //we create a new server
+	course.RegisterCoursesServer(s, &server{})
+
+	log.Printf("server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil { //error while listening
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
