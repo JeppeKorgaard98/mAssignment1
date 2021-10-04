e5917b488cda8eb4670bd850ebbb7fc31955244e
frev <frev@itu.dk>
2021-10-04 12:58:02 +0200

    Would you believe that things work

---
+++ client/client.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"mAssignment1/course" ///Users/Freja/Documents/Programmering/3.semester/disys/Mandatory1/mAssignment1
+
+	"google.golang.org/grpc"
+)
+
+func main() {
+	conn, err := grpc.Dial(":8080", grpc.WithInsecure(), grpc.WithBlock()) //maybe it has to be: localhost:8080
+	if err != nil {                                                        //error can not establish connection
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+
+	client := course.NewCoursesClient(conn)
+
+	response, err := client.GetCourseList(context.Background(), &course.GetCourseListRequest{})
+	if err != nil {
+		log.Fatalf("could not get course list: %v", err)
+	}
+
+	log.Printf("The response from server with list of courses is: %s", response.GetMessage())
+}
