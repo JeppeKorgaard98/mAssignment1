62d9b18c5cd9554df2f69be57a512e3e459aca5d
frev <frev@itu.dk>
2021-10-04 10:41:43 +0200

    Implement course relevant things in server

--- protoAttempt/server/main.go
+++ protoAttempt/server/main.go
@@ -1,11 +1,48 @@
-package main
+package protoAttempt //package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"net"
+	"strconv"
+
+	pb "https://github.com/JeppeKorgaard98/mAssignment1/tree/ProtoAttempt/protoAttempt" //"/Users/Freja/Documents/Programmering/3.semester/disys/Mandatory1/mAssignment1/protoAttempt"
 
 	"google.golang.org/grpc"
 )
+
+type server struct {
+	pb.UnimplementedCourseServer
+}
+
+//DATA HERE
+type Course struct {
+	Id                        int32  `json:"id"`
+	Name                      string `json:"name"`
+	Teacher                   int32  `json:"teacher"`
+	StudentSatisfactionRating int32  `json:"studentSatisfactionRating,omitempty"`
+}
+
+var courses = []Course{
+	{Id: 1, Name: "BDSA", Teacher: 111, StudentSatisfactionRating: 5},
+	{Id: 2, Name: "IDBS", Teacher: 222, StudentSatisfactionRating: 2},
+	{Id: 3, Name: "DSYS", Teacher: 333, StudentSatisfactionRating: 0},
+}
+
+func (s *server) GetCoursesList(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	var num, err = strconv.Atoi(in.GetName())
+	if num <= len(courses) {
+		if err != nil {
+			fmt.Println("error")
+			//error handling
+		}
+		log.Printf("Received: %v", courses[num])
+		return &pb.HelloReply{Message: fmt.Sprint(courses[num].Name) + ";" + fmt.Sprint(courses[num].StudentSatisfactionRating)}, nil
+	} else {
+		return &pb.HelloReply{Message: "error;error"}, nil
+	}
+}
 
 func main() {
 
